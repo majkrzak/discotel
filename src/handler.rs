541c66ccef35bb0231a7ff0070db74d18837347c
Piotr Majkrzak <piotr@majkrzak.dev>
2026-02-01 13:43:22 +0100

    Refactor handlers macro

--- src/handler.rs
+++ src/handler.rs
@@ -7,50 +7,54 @@ use serenity::{
     async_trait,
 };
 
-macro_rules! handler {
-    ($name:ident, $type:ident, ($c:pat, $e:pat),
-    {$(let $init_pat:pat = $init_val:expr;)*},
-    $test:expr, $message:literal,
-    $(($key:literal, $val:expr)),* ) => {
-        async |logger: SdkLogger, $c: Context, event: Event| {
-            if let Event::$type($e) = event {
-                $(let $init_pat = $init_val;)*
-                if $test {
-                    let mut log_record = logger.create_log_record();
-                    log_record.set_event_name(stringify!($name));
-                    log_record.set_observed_timestamp(SystemTime::now());
-                    log_record.set_severity_number(Severity::Trace);
-                    log_record.set_body(format!($message).into());
-                    $(log_record.add_attribute($key, $val);)*
-                    logger.emit(log_record);
+macro_rules! handlers {
+    ( $(
+        $event_name:ident: $event_kind:ident => ($ctx:pat, $mx:pat) {
+            $(let $init_p:pat = $init_e:expr;)*
+        } if ($test:expr) [
+            $message:literal,
+            $($attr_key:literal => $attr_val:expr,)*
+        ];
+    )* ) => {
+        pub struct Handler {
+            pub logger: SdkLogger,
+        }
+
+        impl Handler {
+            $(
+                async fn $event_name (&self, $ctx: Context, ev: Event) {
+                    if let Event::$event_kind($mx) = ev {
+                        $(let $init_p = $init_e;)*
+                        if $test {
+                            let mut log_record = self.logger.create_log_record();
+                            log_record.set_event_name(stringify!($name));
+                            log_record.set_observed_timestamp(SystemTime::now());
+                            log_record.set_severity_number(Severity::Trace);
+                            log_record.set_body(format!($message).into());
+                            $(log_record.add_attribute($attr_key, $attr_val);)*
+                            self.logger.emit(log_record);
+                        }
+                    }
                 }
+            )*
+        }
+
+        #[async_trait]
+        impl RawEventHandler for Handler {
+            async fn raw_event(&self, ctx: Context, ev: Event) {
+                $(self.$event_name (ctx,ev).await;)*
             }
         }
     };
 }
 
-pub struct Handler {
-    pub logger: SdkLogger,
-}
-
-#[async_trait]
-impl RawEventHandler for Handler {
-    async fn raw_event(&self, ctx: Context, ev: Event) {
-        let handlers = vec![handler!(
-            mesage_create,
-            MessageCreate,
-            (ctx, MessageCreateEvent { message, .. }),
-            {
-                let author = message.author.display_name();
-                let channel = message.channel(&ctx).await.unwrap().guild().unwrap().name;
-            },
-            true,
-            "@{author} sent message on #{channel}",
-            ("user.id", message.author.id.to_string()),
-            ("message.content", message.content)
-        )];
-        for handler in handlers {
-            handler(self.logger.clone(), ctx.clone(), ev.clone()).await;
-        }
-    }
+handlers! {
+    message_created: MessageCreate => (ctx, MessageCreateEvent { message, .. }) {
+        let author = message.author.display_name();
+        let channel = message.channel(&ctx).await.unwrap().guild().unwrap().name;
+    } if (true) [
+        "@{author} sent message on #{channel}",
+        "user.id" => message.author.id.to_string(),
+        "message.content" => message.content,
+    ];
 }
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .logger("discolog");
 
     let mut client = ClientBuilder::new(config.discord_token, GatewayIntents::all())
-        .raw_event_handler(Handler{logger})
+        .raw_event_handler(Handler { logger })
         .await?;
 
     client.start().await?;
